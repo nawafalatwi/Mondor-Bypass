15a52761c04acb027c74943bfbcb6318a3c551a7
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2023-02-27 11:50:59 +0700

    Inference using onnx backend

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-# from src.model import train
-# train.train()
+import sys
 
-from src.interact import main
-main.start()
+match sys.argv[1]:
+    case "train":
+        from src.build.model import train
+        train.train()
+
+    case "export":
+        from src.build.model import export
+        export.run_export()
+
+    case "torch_infer":
+        from src.deploy.model.infer_torch import inference
+        from src.deploy.interact import main
+        main.start(inference)
+
+    case "onnx_infer":
+        from src.deploy.model.infer_onnx import inference
+        from src.deploy.interact import main
+        main.start(inference)

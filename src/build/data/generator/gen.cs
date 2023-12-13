fa1bb4d22795eb5a745540c0131907a05a374759
nawafalatwi <148423138+nawafalatwi@users.noreply.github.com>
2023-12-13 14:34:36 +0300

    Update gen.cs

--- src/build/data/generator/gen.cs
+++ src/build/data/generator/gen.cs
@@ -17,13 +17,12 @@ namespace gen
         {
             CaptchaImage cac = new CaptchaImage();
             for (int i=0; i<(1<<17); i++){
-                cac.Width = 105;
-                cac.Height = 32;
+                cac.Width = 250;
+                cac.Height = 60;
                 cac.TextLength = 6;
-                cac.FontColor = Color.FromArgb(100, 100, 100); // (100, 100, 100)
-                cac.LineNoise = CaptchaImage.lineNoiseLevel.High;
-                cac.BackgroundNoise = CaptchaImage.backgroundNoiseLevel.None;
-
+                cac.FontColor = Color.FromArgb(84, 156, 0); // (100, 100, 100)
+                cac.LineNoise = CaptchaImage.lineNoiseLevel.None;
+                cac.BackgroundNoise = CaptchaImage.backgroundNoiseLevel.Low;
                 Bitmap bm = cac.RenderImage();
                 bm.Save("tmp/" + cac.Text + i.ToString() + ".png", ImageFormat.Png);
                 if (i % 1000 == 0){
